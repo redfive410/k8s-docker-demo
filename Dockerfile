74e2143d6a22fac67cadedc34d36c65f2ac35d23
redfive410 <mrjimmy410@gmail.com>
2020-06-06 14:23:12 -0700

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+#Dockerfile
+FROM python:3.7.0a2-alpine3.6
+COPY task.py /tmp
+CMD python /tmp/task.py
---
+++ task.py
@@ -0,0 +1,6 @@
+#task.py
+import time
+
+print('Task starting...')
+time.sleep(5)
+print('Task ended, took 5 seconds')
